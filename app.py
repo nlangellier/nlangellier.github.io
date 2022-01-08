64c2e0a18783b747bf6d63e2ed64ec531ce9e529
Nick Langellier <nick@videa.ai>
2022-01-08 10:11:46 -0600

    apply random hint and add docstrings

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import random
+from typing import Union
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import FileResponse
@@ -9,10 +12,31 @@ app.mount(path='/front-end',
 
 
 @app.get('/')
-def home():
+def home() -> FileResponse:
+    """
+    Loads the 2048 Infinite home page.
+
+    Returns:
+    - FileResponse: front-end/index.html
+    """
+
     return FileResponse('front-end/index.html')
 
 
-@app.get('/items/{item_id}')
-def read_item(item_id: int, q: str | None = None):
-    return {'item_id': item_id, 'q': q}
+@app.get('/hint')
+def get_ai_hint(rows: int, columns: int) -> dict[str, Union[str, int]]:
+    """
+    Retrieves the next move by the AI model as a hint from a given board state.
+
+    Args:
+    - **rows** (int): each item must have a name
+    - **columns** (int): a long description
+
+    Returns:
+    - dict[str, Union[str, int]]: the hint and the rows and columns
+    """
+
+    return {'rows': rows,
+            'columns': columns,
+            'hint': random.choice(['up', 'down', 'left', 'right']),
+            'note': 'This feature is not fully implemented. Hint is random'}
