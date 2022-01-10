4bcde5b4d547ef2c057390dd47b9bb53c1aec111
Nick Langellier <nick@videa.ai>
2022-01-10 10:28:13 -0600

    add data validation

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ from getpass import getpass
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from pymongo import MongoClient
+
+from .constants import MIN_ROWS_COLUMNS, MAX_USERNAME_LENGTH, MAX_ROWS_COLUMNS
 
 app = FastAPI()
 app.mount(path='/front-end',
@@ -27,14 +29,26 @@ db = mongo_client['2048Infinite']
 
 
 class GameState(BaseModel):
-    values: list[list[int]]
+    values: list[list[int]] = Field(default=None,
+                                    description='The state of the game board',
+                                    min_items=MIN_ROWS_COLUMNS,
+                                    max_items=MAX_ROWS_COLUMNS,
+                                    ge=0)
 
 
 class GameOverInfo(BaseModel):
-    name: str = 'Anonymous'
-    score: int
-    rows: int
-    columns: int
+    name: str = Field(default='Anonymous',
+                      description='The username of the player',
+                      max_length=MAX_USERNAME_LENGTH)
+    score: int = Field(default=None,
+                       description='The final score of the game',
+                       ge=0)
+    rows: int = Field(default=None,
+                      description='Number of rows of the game board',
+                      ge=MIN_ROWS_COLUMNS, le=MAX_ROWS_COLUMNS)
+    columns: int = Field(default=None,
+                         description='Number of columns of the game board',
+                         ge=MIN_ROWS_COLUMNS, le=MAX_ROWS_COLUMNS)
 
 
 @app.get(path='/')
