985e2f550a3224e9b4b89cc26b1b5a02162d6ce2
Nick Langellier <nick@videa.ai>
2022-01-14 10:43:15 -0500

    refactor src directory structure

--- src/back_end/constants.py
+++ src/back_end/constants.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 
 DIRPATH_REPO = Path(__file__).parent.parent.parent
-DIRPATH_FRONT_END = DIRPATH_REPO / 'src' / 'front-end'
+DIRPATH_FRONT_END = DIRPATH_REPO / 'src' / 'front_end'
 DIRPATH_IMAGES = DIRPATH_REPO / 'images'
 
 MIN_ROWS_COLUMNS = 3
 MAX_ROWS_COLUMNS = 6
 
 MAX_USERNAME_LENGTH = 50
+
+NEW_TILE_VALUES = [1, 2]
+NEW_TILE_PROBABILITIES = [0.9, 0.1]
