d483af7b0ffb9c10943ec482e255789bedda3717
Nick Langellier <nick@videa.ai>
2022-01-16 08:41:48 -0500

    update /hint route

--- src/back_end/app.py
+++ src/back_end/app.py
@@ -73,24 +73,21 @@ def move_tiles(uuid: int, direction: Direction) -> MoveResponse:
     return MoveResponse(nextTile=active_games[uuid].tile_creation_history[-1])
 
 
-@app.post(path='/hint')
-def get_ai_hint(game_state: GameState) -> dict[str, str]:
+@app.get(path='/hint', response_model=Direction)
+def get_hint(uuid: int) -> Direction:
     """
-    Retrieves the next move by the AI model as a hint from a given board state.
+    Computes the next move the AI model would take from the current game state.
 
     Args:
-    - **game_state** (GameState): The board state. The values correspond to the
-        base 2 logarithm of the text displayed on the tiles. Empty cells are
-        given the value 0.
+    - **uuid** (int): The game ID.
 
     Returns:
-    - dict[str, str]: The returned hint.
+    - Direction: The next move the AI model would take. One of {"left", "up",
+        "right", "down"}.
     """
 
-    game_state = game_state.dict()['values']
-    logger.info(f'{game_state = }')
-    return {'hint': random.choice(['up', 'down', 'left', 'right']),
-            'note': 'This feature is not yet implemented. The hint is random.'}
+    logger.info(f'This method is not yet implemented.')
+    return random.choice(list(Direction))
 
 
 @app.post(path='/add-score')
