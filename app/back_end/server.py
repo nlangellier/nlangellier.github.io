f032103ed9a1b34f9459481cb2c3b087aff44406
Nick Langellier <nick@videa.ai>
2022-01-24 08:01:16 -0500

    formatting

--- app/back_end/server.py
+++ app/back_end/server.py
@@ -7,12 +7,14 @@ from .constants import DIRPATH_FRONT_END, DIRPATH_IMAGES
 from .routes import game, home, leader_board, ping
 
 app = FastAPI()
+
 app.mount(path='/front-end',
           app=StaticFiles(directory=DIRPATH_FRONT_END),
           name='front-end')
 app.mount(path='/images',
           app=StaticFiles(directory=DIRPATH_IMAGES),
           name='images')
+
 app.include_router(router=home.router)
 app.include_router(router=ping.router)
 app.include_router(router=game.router)
